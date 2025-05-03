e1b1f166e286c2c50d266028a1534ec4129c7ef0
Padron Millan Luis <luis57711994@gmail.com>
2025-05-02 22:55:29 -0400

    chore: dale

--- components/map-component.tsx
+++ components/map-component.tsx
@@ -2,19 +2,21 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { LatLngBoundsExpression, LatLngExpression } from 'leaflet';
 
-export default function MapComponent() {
-    const bounds: [[number, number]] = [
-        [-33.4522813, -70.6083369], // Coordenada suroeste
-      ];
+export default function MapComponent() {    
+    const bounds: LatLngBoundsExpression = [
+        [-33.4522813, -70.6083369], 
+    ];
+    const position: LatLngExpression = [-33.4522813, -70.6083369];
   return (
     <MapContainer bounds={bounds} style={{ height: '400px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={bounds}>
+      <Marker position={position}>
         <Popup>
-          Un popup en Leaflet.
+          Aqui nos puedes encontrar
         </Popup>
       </Marker>
     </MapContainer>
